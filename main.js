d8e73de5cbd5f62252eee92558d6959b4527ec82
Mikolaj Figurski <meek.f80@gmail.com>
2022-11-15 15:31:12 -0500

    Load data

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert("Loaded main.js");
+
+// if document is ready
+document.addEventListener("DOMContentLoaded", main);
+
+async function main() {
+  // get the canvas element
+  const canvas = document.getElementById("student-performance-viz-canvas");
+
+  data = await d3.csv("data/StudentsPerformance.csv")
+  console.log(data)
+}
