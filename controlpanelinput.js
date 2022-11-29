34d938b77d13fcbac4a8ac6c3170152cd1ed5f78
Mikolaj Figurski <meek.f80@gmail.com>
2022-11-28 21:29:30 -0500

    Add naive splitting interactions

---
+++ controlpanelinput.js
@@ -0,0 +1,48 @@
+
+function ControlPanelInput(data, onChange, {
+  columns = data.columns ?? Object.keys(data[0]), // array of column names from data to allow selecting
+  runOnChange = true, // whether to run onChange when the control panel is first created
+  defaultValues = {}, // default values for the control panel
+  wrappingClass = "control-panel-input", // class to apply to the wrapping div
+} = {}) {
+  const node = document.createElement("div")
+  node.classList.add("control-panel")
+
+  // STATIC struct holding the state of the control panel
+  const INPUT_VALUES = defaultValues
+
+  // Create each checkbox input for all columns of data
+  columns.forEach(column => {
+    if (typeof data[0][column] !== 'string') {
+      console.warn(`Column ${column} is not a string, so it will not be included in the control panel.`)
+      return
+    }
+
+    // setup value
+    INPUT_VALUES[column] = INPUT_VALUES[column] ?? false
+
+    // create checkbox input elements
+    const wrapper = document.createElement("div")
+    wrapper.classList.add(wrappingClass)
+    node.appendChild(wrapper)
+    const input = document.createElement("input")
+    input.type = "checkbox"
+    input.id = `${column}-checkbox`
+    input.checked = INPUT_VALUES[column] ?? true
+    wrapper.appendChild(input)
+    const label = document.createElement("label")
+    label.setAttribute("for", input.id)
+    label.textContent = column
+    wrapper.appendChild(label)
+
+    // add event listener to update INPUT_VALUES
+    input.addEventListener("change", (e) => {
+      INPUT_VALUES[column] = e.target.checked
+      onChange(INPUT_VALUES)
+    })
+  })
+
+  // run onChange with the initial values
+  if (runOnChange) onChange(INPUT_VALUES)
+  return { node }
+}
--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 
-// if document is ready
-document.addEventListener("DOMContentLoaded", main);
+const onSelectionChanged = (plotNode, data) => (selected) => {
+  // Update the plot with the selected values
+  console.log(selected)
+  const scpltMatrix = ScatterplotMatrix(data, {
+    columns: ["reading score", "writing score", "math score"],
+    domain: [[0, 100], [0, 100], [0, 100]],
+    z: d => {
+      let sum = 0
+      for (const key in selected) {
+        if (selected[key]) sum += d[key]
+      }
+      return sum
+    },
+  })
+  plotNode.innerHTML = ""
+  plotNode.appendChild(scpltMatrix)
+}
 
 async function main() {
-  const plotNode = document.getElementById("plot");
   const data = await d3.csv("data/StudentsPerformance.csv")
-  // console.log(data)
 
-  const scplt_matrix = ScatterplotMatrix(data, {
-    columns: ["reading score", "writing score", "math score"],
-    domain: [[0,100], [0,100], [0,100]],
-    z: d => d.gender,
+  // Set up the plot
+  const plotNode = document.getElementById("plot")
+
+  // Set up the control panel
+  const panelNode = document.getElementById("plot-input")
+  const controlPanel = ControlPanelInput(data, onSelectionChanged(plotNode, data), {
+    columns: ["gender", "race/ethnicity", "parental level of education", "lunch", "test preparation course"],
   })
-  plotNode.appendChild(scplt_matrix)
+  panelNode.appendChild(controlPanel.node)
 }
+
+// if document is ready
+document.addEventListener("DOMContentLoaded", main);
